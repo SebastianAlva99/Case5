2fc74c2c5f3fd082dd1c4d676dd06ae00a1f7cd3
SebastianAlva99 <sp.company.sv@gmail.com>
2025-05-14 07:26:09 +0200

    template done

---
+++ app/public/includes/header.php
@@ -0,0 +1,3 @@
+<header>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/31/Webysther_20160423_-_Elephpant.svg/1200px-Webysther_20160423_-_Elephpant.svg.png" alt="logotype">
+</header>
---
+++ app/public/includes/nav.php
@@ -0,0 +1,4 @@
+<!-- navigering mellan olika filer / sidor -->
+<nav>
+    <a href="/">Startsida</a>
+</nav>
